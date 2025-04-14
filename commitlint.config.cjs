2622ebafeb9bd5983ef2c53a83ee4223c973fd45
aliyun4265149639 <liang_double00@163.com>
2025-04-14 16:55:54 +0800

    fix: 内容规范优化

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,8 +1,19 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  // rules: {
-  //   'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
-  //   'subject-case': [0] // 允许任意大小写
-  // },
-  // helpUrl: 'git提交规范报错请查看根目录下的commitlint.config.js文件'
-}
+  rules: {
+    'type-enum': [
+      2, // 2 表示必须符合规则
+      'always',
+      [
+        '✨', // 新功能
+        '🐛', // Bug 修复
+        '⏪', // 代码合并/拆分
+        '📦', // 打包相关
+        '🦑', // 样式/功能优化
+        // 也可以保留传统类型（可选）
+        'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'
+      ]
+    ],
+    'subject-case': [0] // 允许任意大小写
+  }
+};
