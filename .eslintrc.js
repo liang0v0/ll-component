3b4356e646fc04f151e7e2c745cbec849b4c088e
aliyun4265149639 <liang_double00@163.com>
2025-04-14 16:13:45 +0800

    test

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended', // add
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  plugins: ['@typescript-eslint', 'prettier', 'vue'] // add
+}
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    'subject-case': [0] // 允许任意大小写
+  },
+  helpUrl: 'git提交规范报错请查看根目录下的commitlint.config.js文件'
 }
